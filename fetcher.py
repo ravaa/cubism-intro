3fa4bdbf1b3b3ca8bde57f7ac66251a450c101dc
Erik Blas <erik.blas@socrata.com>
2014-12-10 14:27:39 -0800

    fixing where fetcher stores json

--- fetcher.py
+++ fetcher.py
@@ -12,12 +12,12 @@ for stock in stocks_list:
 
 	page       = request.urlopen(base_url % stock.upper()).read()
 	price_data = page.decode().split('\n')[8:-1]
-	
+
 	for line in price_data:
 		ts, price = line.split(',')
 		ts_data.append([int(ts), float(price)])
 
-	with open('data/%s.json' % stock.upper(), 'w') as f:
+	with open('static/%s.json' % stock.upper(), 'w') as f:
 		json.dump(ts_data, f)
 
 	print("Fetched data for %s" % stock.upper())
